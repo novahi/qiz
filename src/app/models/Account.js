5741140f4e0bca889782bfacb9adf638928d33f4
novahi <nguoidungemail1@gmail.com>
2022-01-05 23:39:15 +0700

    bug 4

--- src/app/models/Account.js
+++ src/app/models/Account.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
-mongoose.plugin(slug);
-
+require('dotenv').config()
 //Connect to User Accounts Database
 const connect = mongoose.createConnection(process.env.ACCOUNT_URI, {
   useNewUrlParser: true,
